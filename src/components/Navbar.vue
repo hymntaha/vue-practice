1385d3ee8ac3f35cb4bc6fc3d53345c52e75143e
taha uygun <tahauygun@gmail.com>
2020-02-29 12:13:41 -0800

    adding axios

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
   <nav>
     <div class="nav-wrapper">
       <div class="container">
-        <a href="#" class="brand-logo">Vue Tutorial</a>
+        <router-link to="/" class="brand-logo">Vue Tutorial</router-link>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/about">About</router-link></li>
         </ul>
       </div>
     </div>
