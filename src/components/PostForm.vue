c1216f49f3c1c72cf15871e60c72774147134bf5
taha uygun <tahauygun@gmail.com>
2020-02-29 13:24:06 -0800

    work on post form

---
+++ src/components/PostForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form v-if="!loading" class="form" v-on:submit="onsubmit">
+    <div class="input-field">
+      <label for="title">Title</label>
+      <input type="text" name="title" v-model="title" class="validate" />
+      <span class="helper-text" data-error="Title must not be empty"></span>
+    </div>
+    <div class="input-field">
+      <label for="body">Title</label>
+      <input type="text" name="body" v-model="body" class="validate">
+      <span class="helper-text" data-error="Body must not be empty"></span>
+
+    </div>
+    <button class="waves-effect waves-light btn"></button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
