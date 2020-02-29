b5ad48a2143ef1c95e3d7c3ee720039a94c65622
taha uygun <tahauygun@gmail.com>
2020-02-29 04:01:46 -0800

    adding navbar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <h2>App</h2>
+  <div>
+    <Navbar />
+    <h2>App</h2>
+  </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../node_modules/materialize-css/dist/js/materialize.min.js';
 
 export default {
   name: 'App',
+  components: {
+    Navbar,
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <div class="nav-wrapper">
+      <div class="container">
+        <a href="#" class="brand-logo">Vue Tutorial</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
