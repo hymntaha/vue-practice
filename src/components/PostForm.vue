5373ad1772af9cdc15a0cea6801be5bf4b13993e
taha uygun <tahauygun@gmail.com>
2020-03-01 14:35:52 -0800

    adding validation in input

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -2,12 +2,12 @@
   <form v-if="!loading" class="form" v-on:submit.prevent="onsubmit">
     <div class="input-field">
       <label for="title">Title</label>
-      <input type="text" name="title" v-model="title" class="validate" />
+      <input type="text" name="title" v-model="title" :class="{errors.title ? 'invalid': 'validate'}" />
       <span class="helper-text" data-error="Title must not be empty"></span>
     </div>
     <div class="input-field">
       <label for="body">Title</label>
-      <input type="text" name="body" v-model="body" class="validate">
+      <input type="text" name="body" v-model="body" :class="{errors.body ? 'invalid': 'validate'}">
       <span class="helper-text" data-error="Body must not be empty"></span>
 
     </div>
